b9400f960c6ff13991d0eaf32d6d6ea61a89bc79
Морева Екатерина Ивановна <moreva.ei@edu.spbstu.ru>
2021-05-13 19:50:18 +0300

    get rid of redundant files

--- app/src/main/java/ru/ok/technopolis/training/personal/utils/recycler/adapters/ItemNAvigationAdapter.kt
+++
@@ -1,4 +0,0 @@
-package ru.ok.technopolis.training.personal.utils.recycler.adapters
-
-class ItemNAvigationAdapter {
-}
--- app/src/main/java/ru/ok/technopolis/training/personal/viewholders/ItemNavigationViewHolder.kt
+++
@@ -1,4 +0,0 @@
-package ru.ok.technopolis.training.personal.viewholders
-
-class ItemNavigationViewHolder {
-}
