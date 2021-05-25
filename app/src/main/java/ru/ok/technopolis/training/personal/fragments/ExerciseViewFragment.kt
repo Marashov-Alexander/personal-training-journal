f07adab2a4b1e4c884f984277fa04e0d6dfd3a16
Marashov_Alexander <sanYAMarashov>
2021-05-25 15:15:03 +0300

    Media loaders at exercise and workout view fragments

--- app/src/main/java/ru/ok/technopolis/training/personal/fragments/ExerciseViewFragment.kt
+++ app/src/main/java/ru/ok/technopolis/training/personal/fragments/ExerciseViewFragment.kt
@@ -5,13 +5,13 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
-import android.widget.ImageSwitcher
 import android.widget.ImageView
 import android.widget.Spinner
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_view_exercise.view.*
+import kotlinx.android.synthetic.main.item_media_viewer.view.*
 import kotlinx.android.synthetic.main.view_appbar.*
 import ru.ok.technopolis.training.personal.R
 import ru.ok.technopolis.training.personal.db.entity.ParameterEntity
@@ -27,19 +27,16 @@ import ru.ok.technopolis.training.personal.utils.recycler.adapters.BundleAdapter
 import ru.ok.technopolis.training.personal.utils.recycler.adapters.ParameterAdapter
 import ru.ok.technopolis.training.personal.viewholders.BundleItemViewHolder
 import ru.ok.technopolis.training.personal.viewholders.ParameterViewHolder
-
 import java.sql.Time
 
 class ExerciseViewFragment : BaseFragment(), ParameterDialogFragment.ParameterDialogListener {
     private var exerciseShortInfoRecycler: RecyclerView? = null
-    private var imageSwitcher: ImageSwitcher? = null
+    private var imageSwitcher: RecyclerView? = null
     private var startButton: ImageView? = null
     private var shareButton: ImageView? = null
     private var downloadsNumber: TextView? = null
     private var raiting: TextView? = null
     private var level: Spinner? = null
-    private var nextSwitch: ImageView? =null
-    private var prevSwitch: ImageView? = null
     private var authorName: TextView? = null
     private var redactorName: TextView? = null
     private var parametersRecycler: RecyclerView? = null
@@ -55,8 +52,6 @@ class ExerciseViewFragment : BaseFragment(), ParameterDialogFragment.ParameterDi
         downloadsNumber = view.downloads_number
         raiting = view.rank_number
         level = view.level_value
-        nextSwitch = view.next_image_btn
-        prevSwitch = view.prev_image_btn
         authorName = view.author_name
         redactorName = view.redactor_name
         parametersRecycler = view.exercise_param_list
